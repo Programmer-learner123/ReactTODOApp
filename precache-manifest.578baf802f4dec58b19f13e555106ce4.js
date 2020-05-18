98dc8c5f67c2b5470899803c527602a54d8ea972
Programmer-learner123 <learnerakansha@gmail.com>
2020-05-18 22:52:54 +0530

    Updates

---
+++ precache-manifest.578baf802f4dec58b19f13e555106ce4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a436969bc51ca1cf54153e4679408252",
+    "url": "/ReactTODOApp/index.html"
+  },
+  {
+    "revision": "0054182ff56e1dbd7fb1",
+    "url": "/ReactTODOApp/static/css/main.0ad62635.chunk.css"
+  },
+  {
+    "revision": "8fb81934628b68e2648d",
+    "url": "/ReactTODOApp/static/js/2.0efba08a.chunk.js"
+  },
+  {
+    "revision": "0054182ff56e1dbd7fb1",
+    "url": "/ReactTODOApp/static/js/main.e4a1a1d6.chunk.js"
+  },
+  {
+    "revision": "a711cf09102198480983",
+    "url": "/ReactTODOApp/static/js/runtime~main.e8a1a982.js"
+  },
+  {
+    "revision": "6d0a642c7d230d929229b9bfba740461",
+    "url": "/ReactTODOApp/static/media/ap.6d0a642c.png"
+  }
+]);
